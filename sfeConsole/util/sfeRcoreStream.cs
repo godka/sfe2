1c71b5fd29221a3e39645b50a857a7f694397204
sty <scarsty@gmail.com>
2020-04-06 19:57:32 +0800

    kgpic有错误,改为xlsx

--- sfeConsole/util/sfeRcoreStream.cs
+++ sfeConsole/util/sfeRcoreStream.cs
@@ -169,20 +169,20 @@ namespace imzopr.rdata
                 Ridx = new List<string>();
             }
         }
-        public static void SaveRToXls(Stream filegrp, string fileidx, string inifilepath,string xlsFile)
+        public static void SaveRToXls(Stream filegrp, string fileidx, string inifilepath, string xlsFile)
         {
-            HSSFWorkbook newBook = new HSSFWorkbook();
+            XSSFWorkbook newBook = new XSSFWorkbook();
 
             List<object> rows = new List<object>();
             List<RdataType> datalist = ReadRanger(filegrp, fileidx, inifilepath);
             for (int k = 0; k < datalist.Count; k++)
             {
                 object[,] array = datalist[k].ToArray();
-                HSSFSheet newSheet = (HSSFSheet)newBook.CreateSheet(datalist[k].TypeName);//新建工作簿
+                XSSFSheet newSheet = (XSSFSheet)newBook.CreateSheet(datalist[k].TypeName);//新建工作簿
                 
                 for (int i = 0; i < array.GetLength(1); i++)
                 {
-                    HSSFRow newRow = (HSSFRow)newSheet.CreateRow(i);//创建行
+                    XSSFRow newRow = (XSSFRow)newSheet.CreateRow(i);//创建行
                     for (int j = 0; j < array.GetLength(0); j++)
                     {
                         if(array[j,i].GetType() == typeof(String))
@@ -191,7 +191,7 @@ namespace imzopr.rdata
                             newSheet.GetRow(i).CreateCell(j).SetCellValue((int)array[j, i]);
                     }
                 }
-                newSheet.AutoSizeColumn(-2);
+                //newSheet.AutoSizeColumn(-2);
             }
             FileStream fs = new FileStream(xlsFile, FileMode.Create);
             newBook.Write(fs);
@@ -321,7 +321,7 @@ namespace imzopr.rdata
         }
         public static List<RdataType> ReadRanger(Stream filegrp, string fileidx, string inifilepath)
         {
-            try
+            //try
             {
                 List<inilist.R_modify> rini = inilist.ini2list(inifilepath);
                 List<RdataType> RMem = new List<RdataType>();
@@ -434,9 +434,9 @@ namespace imzopr.rdata
                 filegrp.Close();
                 return RMem;
             }
-            catch
+            //catch
             {
-                return null;
+                //return null;
             }
         }
     }
